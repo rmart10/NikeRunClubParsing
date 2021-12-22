b8d1f56bc26a514c96592e2b307e81fde9e90202
Martin, Rich A <rich@richmartinonline.com>
2021-12-21 21:37:34 -0600

    restructured, abstracted google drive functionality into class

--- AnalyzeNRCData.py
+++ AnalyzeNRCData.py
@@ -4,12 +4,27 @@ from zipfile import ZipFile
 import json
 import datetime as dt
 import pandas as pd
+from numpy.lib import function_base
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+## dropbox library
 from dropbox.dropbox_client import Dropbox
 
-appkey = '3d3lqdzah65hct6'
-appsecret = 'eybf9o1k5avf1v1'
-token = '{}'
+from googledriveclient import GoogleDriveClient
+
+
+###GOOGLE SHEETS ID, friendly name and description OF THE SHEET YOU ARE REPLACING.
+spreadsheetId = '15KWFLCR2HglBihbCWJ-Lm_FswD3Pvk7sTKtdKNgMeq0' 
+googleDriFileFriendlyName = 'Nike Run Club Data Extract'
+googleDriveFileDescription = 'A Pandas derived collection of Richs run data'
+parentFolderId = "1-rRiTjAWVfsty7vhLCud9OrMg0sXW9Xp" ## only used when creating a new file, can be passed into constructor and is ignored in GoogleDriveClient class
+ 
+
+###DROPBOX CREDENTIALS
+appkey = ##
+appsecret = ##
+token = ##
 #### YOU MUST CREATE AN APP ON DROPBOX THAT IS FULL SCOPED!!!!!!!
 
 
@@ -20,13 +35,85 @@ dbxPath = '/Apps/RunGap/export'
 # #folder relative to this script you want to store retrieved files in.
 dataPath = 'Data/'
 
+# #file name of processed, combined results
+outputFileName = 'NRCDataCombined.csv'
+
 ##current folder path of script
 scriptPath = os.getcwd()
 scriptPath = scriptPath.replace("\\","/")
 
 
-##dictionary which will hold all runs
-runs = {}
+
+### columns list for data frame.
+columns = [
+'runType',
+'distance_Miles',
+'outStartDate',
+'outStartTime',
+'distance_KM',
+'timeZone',
+'duration_Seconds',
+'avgSpeed_MetersSec',
+'avgSpeed_MPH',
+'maxSpeed_MetersSec',
+'maxSpeed_MPH',
+'calories',
+'avgHR',
+'maxHR',
+'elevationGain',
+'elevationLoss',
+'minElevation',
+'maxElevation',
+'avgCadence',
+'maxCadence',
+'steps',
+'startLatitude',
+'startLongitude',
+'avgTemp'
+]
+
+
+## blank df which will hold runs.
+dfRuns = pd.DataFrame(columns=columns)
+
+cols = ['STATION', 'NAME', 'DATE', 'AWND', 'PGTM', 'TAVG', 'TMAX', 'TMIN',
+       'TOBS', 'WDF2', 'WDF5', 'WSF2', 'WSF5']
+dfWeather = pd.DataFrame(columns=cols)
+
+
+## bool that will control whether or not we make a call to dropbox to fetch all NRC files
+refreshData = False
+
+## bool that will control whether we refresh the data on Google Drive
+refreshGoogleDriveData = False
+
+
+def loadWeatherDataFrame():
+    '''Function reads csv weather_raw in same app directory and populates already established dfWeather DataFrame.
+    Accepts/Requires 0 parameters:''' 
+
+    ##read csv
+    dfWeatherRaw = pd.read_csv('weather_raw.csv')   
+
+    ##new df after dropping rows with no avg temp
+    dfWeatherClean = dfWeatherRaw.dropna(subset=['TAVG'])
+
+    ##cast date column to date
+    dfWeatherClean['DATE'] = pd.to_datetime(dfWeatherClean['DATE'])
+
+    for index, row in dfWeatherClean.iterrows():
+        dfWeather.loc[index] = pd.Series(row)     
+
+
+def lookupAvgWeatherByDate(df: pd.DataFrame, datein: dt.datetime):  
+    '''Function will lookup the avg temp value in data frame passed in and return the value.
+    Accepts/Requires 2 parameters: df = dfWeather data frame, datein: YYYY-MM-DD datetime object'''      
+
+    ## look up a value in the df using .query
+    dfresult = df.query("DATE =='" + str(datein)+"'")    
+
+    ##RETURN A VALUE FROM RESULT    
+    return str(dfresult['TAVG'].values)
 
 
 
@@ -35,26 +122,38 @@ def downloadData(dbx: Dropbox):
     Accepts/Requires 1 parameter: dbx = Dropbox object reference'''    
     
     #populate variable(class 'dropbox.files.ListFolderResult) with all files available for download.
-    result = dbx.files_list_folder(dbxPath)    
+    result = dbx.files_list_folder(dbxPath) 
+
+    print("###### Fetching new data...")  
+
+    print("###### Found ", len(result.entries), " files...")  
 
     ##iterate each entry found and download to the relative path specified in dataPath
     ##will overwrite existing files.
     for entry in result.entries:
         dbx.files_download_to_file(dataPath+entry.name,entry.path_lower)
 
-
     ##unzip each csv file from archie
     for fileName in os.listdir(dataPath):
         print("Extracting ",fileName)
-        with ZipFile(scriptPath + '/' + dataPath + fileName,'r') as zipObj:
-            zipObj.extractall(dataPath)
+        if fileName.endswith('.zip'):
+            with ZipFile(scriptPath + '/' + dataPath + fileName,'r') as zipObj:
+                try:
+                    zipObj.extractall(dataPath)
+                except Exception as e:
+                    print("Error", e)
+    print("###### Done downloading and extracting files...") 
+
+
 
 
 def convertKMToMiles(inKM):
+    '''Function will convert KM to miles''' 
     miles=  inKM * .62137119
     return miles
 
 def convertMetersSecondToMPH(inMS):
+    '''Function will convert meters per second to mph''' 
     mph = inMS * 2.23693629
     return mph
 
@@ -72,74 +171,66 @@ def calculateRunType(inRunDict:dict):
 
 
 
-# ##CONSTRUCT AND INIT YOUR DROPBOX OBJECT
-#dbx = dropbox.Dropbox(token)
 
-##download/refresh local directory with Nike Run Club data from Dropbox
-#downloadData(dbx)
+################################ LOAD/CLEAN WEATHER DATA FRAME
+loadWeatherDataFrame()
 
-
-##this needs to be a dict or something.
-runType = ""
-distance_Miles = ""
-outStartDate = ""
-outStartTime = ""
-distance_KM = ""
-timeZone = ""
-duration_Seconds = ""
-avgSpeed_MetersSec = ""
-avgSpeed_MPH = ""
-maxSpeed_MetersSec = ""
-maxSpeed_MPH = ""
-calories = ""
-avgHR = ""
-maxHR = ""
-elevationGain = ""
-elevationLoss = ""
-minElevation = ""
-maxElevation = ""
-avgCadence = ""
-maxCadence = ""
-steps = ""
+if refreshData == True:
+    ##CONSTRUCT AND INIT YOUR DROPBOX OBJECT
+    dbx = dropbox.Dropbox(token)
+    #download/refresh local directory with Nike Run Club data from Dropbox
+    downloadData(dbx)
+else:
+    print("###### Not fetching new data...")
 
 
+##init vars used for appending to df 
+runType,distance_Miles,outStartDate,outStartTime,distance_KM,timeZone,duration_Seconds,avgSpeed_MetersSec, \
+avgSpeed_MPH,maxSpeed_MetersSec,maxSpeed_MPH,calories,avgHR,maxHR,elevationGain,elevationLoss,minElevation, \
+maxElevation,avgCadence,maxCadence,steps,startLatitude,startLongitude,avgTemp = "","","","","","","","","","","","","","","","","","","","","","","",""
 
 
+print("###### Beginning processing of downloaded, extracted files...")
+indexCounter = 0
 for fileName in os.listdir(dataPath):
     ### limit to metadata files, their detail is suffice
     if fileName.endswith('metadata.json'):
-        print("Reading... ",fileName)
+        #print("Reading... ",fileName)
         
         ## read the entire json string of the file into a dictionary
         with open(scriptPath + '/' + dataPath + fileName,'r') as string:
             file_dict=json.load(string)
-        string.close()
+        string.close()       
 
-        
+
+        ##reset vars used for appending to df (23)
+        runType,distance_Miles,outStartDate,outStartTime,distance_KM,timeZone,duration_Seconds,avgSpeed_MetersSec, \
+        avgSpeed_MPH,maxSpeed_MetersSec,maxSpeed_MPH,calories,avgHR,maxHR,elevationGain,elevationLoss,minElevation, \
+        maxElevation,avgCadence,maxCadence,steps,startLatitude,startLongitude,avgTemp = "","","","","","","","","","","","","","","","","","","","","","","",""
 
         runType = calculateRunType(file_dict)
 
         for key in file_dict:          
             # DATE, TIME, TIMEZONE, DURATION, DISTANCE, AVG SPEED, MAX SPEED, CALORIES, AVG HR, MAX HR, ELE GAIN, ELE LOSS, MIN ELE, MAX ELE
             #AVGCADENCE, MAXCADENCE, STEPS,
-            #                    
-           
-
-            
-
-            
+            #            
             if key == 'distance':                
                 distance_KM = str(file_dict[key])
                 distance_KM = float(distance_KM) / 1000
                 distance_Miles = convertKMToMiles(distance_KM)
 
             elif key == 'startTime':
-                startTime = file_dict[key]['time']                
+                startTime = file_dict[key]['time'] 
+                               
                 startTime = startTime.replace('Z','').replace('T',' ') ##format the value for constructing date time from pandas...
                 
                 startDateTimeObj = pd.to_datetime(startTime) ##cast to datetime using Pandas               
-                outStartDate = startDateTimeObj.strftime("%Y-%m-%d")
+                outStartDate = startDateTimeObj.strftime("%Y-%m-%d")                          
                 outStartTime = startDateTimeObj.time()  
+
+                ##set temp
+                avgTemp = lookupAvgWeatherByDate(dfWeather,outStartDate)
+                avgTemp = avgTemp.replace('[','').replace(']','').replace('.','')      
 
                 ##try to get timezone
                 try:
@@ -187,30 +278,71 @@ for fileName in os.listdir(dataPath):
 
             elif key == 'steps':
                 steps = file_dict[key]
-                  
+            
+            elif key == 'displayPath':
+                ##run type should be Outdoors/Mobile
+                startLatitude = file_dict[key][0]['lat']
+                startLongitude = file_dict[key][0]['lon']
+            
+
+          
+            
+        row = pd.Series([runType,distance_Miles,outStartDate,outStartTime,distance_KM,timeZone,
+        duration_Seconds,avgSpeed_MetersSec,avgSpeed_MPH,maxSpeed_MetersSec,maxSpeed_MPH
+        ,calories,avgHR,maxHR,elevationGain,elevationLoss,minElevation,maxElevation,
+        avgCadence,maxCadence,steps,startLatitude,startLongitude,avgTemp],index=dfRuns.columns)
+        
+        
+        #append row to dataframe at counters index
+        dfRuns.loc[indexCounter]=row
+        indexCounter +=1 ##increment counter
+
+        
 
             
 
-            #### CONSTRUCT THE DICT BEFORE ADDING TO RUNS DICT
-            data = {'file name':fileName,'run type':runType,'date':outStartDate,'time':outStartTime, 'timeZone': timeZone, 'duration(sec)':duration_Seconds,
-            'distance (km)':distance_KM,'distance (miles)':distance_Miles,'avg speed (meters/second)':avgSpeed_MetersSec,
-            'avg speed (MPH)':avgSpeed_MPH,'max speed (meters/second)':maxSpeed_MetersSec,'max speed (MPH)':maxSpeed_MPH,
-            'calories':calories,'avg HR':avgHR,'max HR':maxHR,'elevation gain':elevationGain,'elevation loss':elevationLoss,
-            'min elevation':minElevation,'max elevation':maxElevation,'avg cadence':avgCadence,'max cadence':maxCadence,'steps':steps}
-            
-            ##ADD THE DICT TO THE RUNS DICT
-            runs[fileName]=data            
-            
-            
-       
+               
+           
+print("###### Completed processing of downloaded, extracted files. See:" ,outputFileName)
+dfRuns.to_csv(outputFileName,header=True)
 
-# for item in runs.items():
-#     print(item)
 
-runsDF = pd.DataFrame.from_dict(runs)
-print(runsDF.head)
-runsDF_Melted = runsDF.unstack().reset_index(name='file name')
-runsDF_Melted.to_csv('melted.csv') ### now its really tall!
+
+if refreshGoogleDriveData == True:
+    ##upload and overwrite file on google drive
+    gDriveClient=GoogleDriveClient(outputFileName,'Nike Run Club Data Extract','A Pandas derived collection of Richs run data','',spreadsheetId)
+    result = gDriveClient.replaceFileFile()
+
+
+############### PLOT OUT SOME HIGH LEVEL AGGREGATES
+
+print("##### Total Runs by Run Type:")
+dfRunTypes = dfRuns.groupby('runType').size()
+print(dfRunTypes)
+dfRunTypes.plot(x="runType",y="Count",kind="bar")
+plt.xlabel('Run Types')
+plt.xticks(rotation=0) ##rotate x axis labels, they were at 90
+plt.ylabel('Run  Counts')
+plt.title('Total Runs by Run Type')
+plt.grid(axis='y') ## show gridlines on x axis only
+plt.show()
+
+
+
+
+# dfCleanAvgHRDistance = dfRuns.dropna(subset=['distance_Miles','avgHR']) ##remove rows from the dfRuns DF that have nulls in either of these columns.
+# dfCleanAvgHRDistance = dfCleanAvgHRDistance.query("distance_Miles >= 1").query("avgHR > 0") ## remove rows where less than 1 mile was run, no HR was captured
+
+# dfCleanAvgHRDistance['distance_Miles'] = dfCleanAvgHRDistance['distance_Miles'].astype(float)
+
+# dfCleanAvgHRDistance['avgHR'] = dfCleanAvgHRDistance['avgHR'].astype(float)
+# sns.regplot(x="avgHR",y="distance_Miles",data=dfCleanAvgHRDistance)
+# plt.title("Linear Regression Model Fit: Average HR and Distance Run")
+# plt.show()
+
+
+
+
 
 
 
